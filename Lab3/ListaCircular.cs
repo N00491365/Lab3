9c5d86837d2420cc1b1657221751bf0a467b9f47
CRISTIAN <CRISTIAN>
2026-09-24 19:10:27 -0500

    Agregar archivos de proyecto.

---
+++ Lab3/ListaCircular.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class ListaCircular
+{
+    private Nodo head; 
+    private Nodo last; 
+
+    public ListaCircular()
+    {
+        head = null;
+        last = null;
+    }
+
+    public void Insertar(int dato)
+    {
+        Nodo nuevo = new Nodo(dato);
+
+        if (head == null) 
+        {
+            head = nuevo;
+            last = nuevo;
+            nuevo.Siguiente = head; 
+        }
+        else
+        {
+            last.Siguiente = nuevo; // El último apunta al nuevo
+            nuevo.Siguiente = head; // El nuevo apunta al primero (circular)
+            last = nuevo;           // Actualizamos el puntero last
+        }
+    }
+}
